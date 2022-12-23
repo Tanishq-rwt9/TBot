e62bfe092c110aa6f291c7ce06cb787f101a07b9
Tanishq Rawat <rawattanishq9002@gmail.com>
2022-12-23 04:26:35 +0000

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+l=input("Hello , I am TBOT\nWhat is your name?")
+print("Hello! "+l)
+print("What can i do for you today?\n")
+print("1.Calculator\n2.Talk with you\n3.Play a game(Stone Paper Scissor)")
