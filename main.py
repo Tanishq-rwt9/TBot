de9328448790e999aff4a6e483049a1230181415
Tanishq Rawat <75470183+Tanishq-rwt9@users.noreply.github.com>
2023-03-20 12:38:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+print('Hello friend')
+l=input('What is your name?')
+print('Oh! Hello ' ,l)
+print('My name is Eva Your friend Robot')
+k=input('Wanna play a game with me?')
+if k=='Yes':
+    print('ok')
+    g = input('Which game you want to play?  1. Stone Paper Scissor')
+if g=='1':
+    print('Ok You go first')
+    u=input()
