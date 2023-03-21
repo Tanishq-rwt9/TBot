e987ba7e8d34000e1ea024c012a0a3b16355be47
tanishq12233 <rawattanishq9002@gmail.com>
2023-03-21 14:20:21 +0530

    Development
    sa

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
+# Introduction of bot
 print('Hello friend')
-l=input('What is your name?')
-print('Oh! Hello ' ,l)
+l=input('What is your name? ')
+print('Oh! Hello ',l)
 print('My name is Eva Your friend Robot')
+# First Input
 k=input('Wanna play a game with me?')
+# Elseif + input
 if k=='Yes':
-    print('ok')
-    g = input('Which game you want to play?  1. Stone Paper Scissor')
+    g = input('''Which game you want to play?
+              1. Stone Paper Scissor 
+              2. Make an Introduction for you''')
+    # Elseif+input
 if g=='1':
     print('Ok You go first')
     u=input('Your Turn')
+   # Elseif+Input
     if u=='Scissor':
         print('Rock')
-        if u=='Paper':
+        print('Better luck next time')
+        if u == 'Rock':
+            print('Paper')
+            print('Better luck next time')
+        if u == 'Paper':
             print('Scissor')
-            if u=='Rock':
-                print('Paper')
-                print('I won ')
-                print('\U0001F923')
+            print('Better luck next time')
+            print('Thanks for trying my program')
+            print('made by Tanishq <3')
+            if g=='2':
+                print('lol')
+
 
 
