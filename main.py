4df6ecad369d24b230684ceb74cf58eb5b8d1a8d
Tanishq Rawat <75470183+Tanishq-rwt9@users.noreply.github.com>
2023-04-16 11:03:37 +0530

    one more update

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
 # Introduction of bot
-print('Hello friend')
-l=input('What is your name? ')
-print('Oh! Hello ',l)
-print('My name is Eva Your friend Robot')
-# First Input
-k=input('Wanna play a game with me?')
-# Elseif + input
-if k=='Yes':
-    g = input('''Which game you want to play?
+try:
+ import random
+ mylist = ["Stone","Paper","Scissor"]
+ print('Hello friend')
+ l=input('What is your name? ')
+ print('Oh! Hello ',l)
+ print('My name is Eva Your friend Robot')
+ # First Input
+ g = input('''Which game you want to play?
               1. Stone Paper Scissor 
               2. Make an Introduction for you''')
     # Elseif+input
-if g=='1':
+ if g=='1':
     print('Ok You go first')
-    u=input('Your Turn')
+    u=input('Your Turn ')
+    print(random.choice(mylist))
    # Elseif+Input
-    if u=='Scissor':
-        print('Rock')
-        print('Better luck next time')
-        if u == 'Rock':
-            print('Paper')
-            print('Better luck next time')
-        if u == 'Paper':
-            print('Scissor')
-            print('Better luck next time')
-            print('Thanks for trying my program')
-            print('made by Tanishq <3')
-        import random
-
-        mylist = ["apple", "banana", "]
-
-        print(random.choice(mylist))
+ print('Better luck next time\n 1:0')
+ print('Thanks for trying my program')
+ print('made by Tanishq <3')
+except:
+    print('Invalid')
