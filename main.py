071803dc75b062ad6b8cb8ce41882e98f1629362
Tanishq Rawat <75470183+Tanishq-rwt9@users.noreply.github.com>
2023-04-20 08:00:37 +0530

    Upada

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 # Introduction of bot
 try:
  import random
- mylist = ["Stone","Paper","Scissor"]
+ mylist = ["Rock","Paper","Scissor"]
  print('Hello friend')
  l=input('What is your name? ')
- print('Oh! Hello ',l)
- print('My name is Eva Your friend Robot')
+ print('Hello!',l)
  # First Input
  g = input('''Which game you want to play?
               1. Stone Paper Scissor 
-              2. Make an Introduction for you''')
+              2. Make an Introduction for you ''')
     # Elseif+input
  if g=='1':
     print('Ok You go first')
     u=input('Your Turn ')
     print(random.choice(mylist))
+    if random.choice=='Scissor':
+       print('lol')
    # Elseif+Input
  print('Better luck next time\n 1:0')
  print('Thanks for trying my program')
