eae8137a726487364581b61d2c9296d0b80a826c
Tanishq Rawat <75470183+Tanishq-rwt9@users.noreply.github.com>
2023-10-17 17:27:44 +0530

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,1 @@
-l=input("Hello , I am TBOT\nWhat is your name?")
-print("Hello! "+l)
-print("What can i do for you today?\n")
-k=input("1.Calculator\n2.Talk with you\n3.Play a game(Stone Paper Scissor)")
-if k==1:
-    print("llulu")
+#Nothing Here
